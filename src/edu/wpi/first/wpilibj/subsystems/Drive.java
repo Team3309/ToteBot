0fbf85115fbc1db59d8d6cb32ebf446bd535783a
Matthew Krager <metoofarmer@yahoo.com>
2015-10-16 22:27:26 -0700

    cheesy drive

--- src/edu/wpi/first/wpilibj/subsystems/Drive.java
+++ src/edu/wpi/first/wpilibj/subsystems/Drive.java
@@ -42,8 +42,9 @@ public class Drive extends Subsystem {
     
     public void runDrive(double throttle, double turn) {
         System.out.println("THROTTLE : " + throttle);
-        setRightSide(throttle - turn);
-        setLeftSide(throttle + turn);
+        cheesyDrive(throttle, turn, true, false); 
+        // 2setRightSide(throttle - turn);
+        // setLeftSide(throttle + turn);
     }
     
     private double skimGain = .25;
@@ -109,8 +110,137 @@ public class Drive extends Subsystem {
         leftMiddle.set(v);
         leftBack.set(v);
     }
+     
+     public double handleDeadband(double val, double deadband) {
+        return (Math.abs(val) > Math.abs(deadband)) ? val : 0.0;
+    }
+     
+    double oldWheel, quickStopAccumulator;
+    private double throttleDeadband = 0.02;
+    private double wheelDeadband = 0.02;
     
+    public static double limit(double v, double limit) {
+        return (Math.abs(v) < limit) ? v : limit * (v < 0 ? -1 : 1);
+    }
     
+     public void cheesyDrive(double throttle, double wheel, boolean isQuickTurn,
+                            boolean isHighGear) {
+        if (DriverStation.getInstance().isAutonomous()) {
+            return;
+        }
+
+        double wheelNonLinearity;
+
+        wheel = handleDeadband(wheel, wheelDeadband);
+        throttle = handleDeadband(throttle, throttleDeadband);
+
+        double negInertia = wheel - oldWheel;
+        oldWheel = wheel;
+
+        if (isHighGear) {
+            wheelNonLinearity = 0.6;
+            // Apply a sin function that's scaled to make it feel better.
+            wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
+                    / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+            wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
+                    / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+        } else {
+            wheelNonLinearity = 0.5;
+            // Apply a sin function that's scaled to make it feel better.
+            wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
+                    / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+            wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
+                    / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+            wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel)
+                    / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+        }
+
+        double leftPwm, rightPwm, overPower;
+        double sensitivity;
+
+        double angularPower;
+        double linearPower;
+
+        // Negative inertia!
+        double negInertiaAccumulator = 0.0;
+        double negInertiaScalar;
+        if (isHighGear) {
+            negInertiaScalar = 4.0;
+            sensitivity = .75;
+        } else {
+            if (wheel * negInertia > 0) {
+                negInertiaScalar = 2.5;
+            } else {
+                if (Math.abs(wheel) > 0.65) {
+                    negInertiaScalar = 5.0;
+                } else {
+                    negInertiaScalar = 3.0;
+                }
+            }
+            sensitivity = .85; // Constants.sensitivityLow.getDouble();
+        }
+        double negInertiaPower = negInertia * negInertiaScalar;
+        negInertiaAccumulator += negInertiaPower;
+
+        wheel = wheel + negInertiaAccumulator;
+        if (negInertiaAccumulator > 1) {
+            negInertiaAccumulator -= 1;
+        } else if (negInertiaAccumulator < -1) {
+            negInertiaAccumulator += 1;
+        } else {
+            negInertiaAccumulator = 0;
+        }
+        linearPower = throttle;
+
+        // Quickturn!
+        if (isQuickTurn) {
+            if (Math.abs(linearPower) < 0.2) {
+                double alpha = 0.1;
+                quickStopAccumulator = (1 - alpha) * quickStopAccumulator
+                        + alpha * limit(wheel, 1.0) * 5;
+            }
+            overPower = 1.0;
+            if (isHighGear) {
+                sensitivity = .8;
+            } else {
+                sensitivity = .8;
+            }
+            angularPower = wheel;
+        } else {
+            overPower = 0.0;
+            angularPower = Math.abs(throttle) * wheel * sensitivity
+                    - quickStopAccumulator;
+            if (quickStopAccumulator > 1) {
+                quickStopAccumulator -= 1;
+            } else if (quickStopAccumulator < -1) {
+                quickStopAccumulator += 1;
+            } else {
+                quickStopAccumulator = 0.0;
+            }
+        }
+
+        rightPwm = leftPwm = linearPower;
+        leftPwm += angularPower;
+        rightPwm -= angularPower;
+
+        if (leftPwm > 1.0) {
+            rightPwm -= overPower * (leftPwm - 1.0);
+            leftPwm = 1.0;
+        } else if (rightPwm > 1.0) {
+            leftPwm -= overPower * (rightPwm - 1.0);
+            rightPwm = 1.0;
+        } else if (leftPwm < -1.0) {
+            rightPwm += overPower * (-1.0 - leftPwm);
+            leftPwm = -1.0;
+        } else if (rightPwm < -1.0) {
+            leftPwm += overPower * (-1.0 - rightPwm);
+            rightPwm = -1.0;
+        }
+        this.setLeftSide(leftPwm );
+        this.setRightSide(rightPwm);
+        
+    }
+     
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
